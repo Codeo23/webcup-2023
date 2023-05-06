29a0d44794af25154a749c9ecea60b3e18c873ba
MiRakMG <mirakmello@gmail.com>
2023-05-07 02:13:54 +0300

    Next Landing Pages

--- front/src/client/pages/landing/index.tsx
+++ front/src/client/pages/landing/index.tsx
@@ -1,6 +1,8 @@
 import React,{useRef,useEffect} from 'react'
 import { Navigation } from '../../layouts/navigation'
 import { Section1 } from './section1'
+import { Section2 } from './section2'
+import { Section3 } from './section3'
 
 type Props = {}
 
@@ -18,17 +20,26 @@ export const Landing = (props: Props) => {
 
   return (
     <div>
-      <div className="overlay" ref={ref}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-      <Navigation/>
-      <Section1/>
+        <div className="overlay" ref={ref}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+      <div className='main_container'>  
+        <Navigation/>
+        <div className='fixed-section'>
+            <Section1/> 
+        </div>
+        <div className='scrollable_section'> 
+            <Section2/>
+            <Section3/>
+            
+        </div>
+      </div>  
     </div>
   )
 }
