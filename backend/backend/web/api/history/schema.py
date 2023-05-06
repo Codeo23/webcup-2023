ac40e9b505067bb7ef533357a0332e4a9c891e95
TianaNanta <andrianjakananta@gmail.com>
2023-05-06 20:20:33 +0300

    add: history endpoint

---
+++ backend/backend/web/api/history/schema.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class HistoryRead(BaseModel):
+    id: int
+    date: str
+    user_id: int
+    dream: str
+    interpretation: str
+    class Config:
+        orm_mode = True
+        
+class HistoryCreate(BaseModel):
+    dream: str
+    interpretation: str
+    user_id: int
+    class Config:
+        orm_mode = True
--- backend/backend/web/api/history/views.py
+++ backend/backend/web/api/history/views.py
@@ -1,4 +1,27 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
+
+from backend.db.models.relationship import HistoryModel
+from backend.db.dao.history_dao import HistoryDAO
+from backend.web.api.history.schema import HistoryRead, HistoryCreate
 
 router = APIRouter()
 
+@router.get("/{user_id}", response_model=List[HistoryRead])
+async def get_all_dreams(
+    user_id: int,
+    limit: int = 10,
+    offset: int = 0,
+    historydao: HistoryDAO = Depends(),
+) -> List[HistoryModel]:
+    """Retrieve all dream objects from the database.
+
+    Args:
+        limit (int, optional): limit of the dream object. Defaults to 10.
+        offset (int, optional): offset of the dream object. Defaults to 0.
+        historydao (HistoryDAO, optional): DAO for the dream history. Defaults to Depends().
+
+    Returns:
+        List[HistoryModel]: List of the dream objects from the database.
+    """
+    return await historydao.get_all_dreams(limit=limit, offset=offset, user_id=user_id)
