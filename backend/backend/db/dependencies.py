6964a84e68055618947e791da5298201fd92a7cd
TianaNanta <andrianjakananta@gmail.com>
2023-05-06 14:35:36 +0300

    update: add get user db function

--- backend/backend/db/dependencies.py
+++ backend/backend/db/dependencies.py
@@ -1,7 +1,11 @@
 from typing import AsyncGenerator
 
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
+
+from backend.db.models.user_model import UserModel
 
 
 async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
@@ -18,3 +22,6 @@ async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]
     finally:
         await session.commit()
         await session.close()
+
+async def get_user_db(session: AsyncSession = Depends(get_db_session)):
+    yield SQLAlchemyUserDatabase(session, UserModel)
