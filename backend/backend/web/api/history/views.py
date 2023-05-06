ba9cf4f2991980efa6ba1b486b55e8e7a20e4d27
TianaNanta <andrianjakananta@gmail.com>
2023-05-06 21:59:48 +0300

    feat: protect route

--- backend/backend/web/api/history/views.py
+++ backend/backend/web/api/history/views.py
@@ -1,17 +1,20 @@
 from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException, status
 
-from backend.db.models.relationship import HistoryModel
 from backend.db.dao.history_dao import HistoryDAO
-from backend.web.api.history.schema import HistoryRead, HistoryCreate
+from backend.db.models.relationship import HistoryModel, UserModel
+from backend.settings import settings
+from backend.web.api.history.schema import HistoryRead
+from backend.services.jwtauth import current_user
 
 router = APIRouter()
 
-@router.get("/{user_id}", response_model=List[HistoryRead])
+@router.get("/", response_model=List[HistoryRead])
 async def get_all_dreams(
-    user_id: int,
     limit: int = 10,
     offset: int = 0,
+    user: UserModel = Depends(current_user),
     historydao: HistoryDAO = Depends(),
 ) -> List[HistoryModel]:
     """Retrieve all dream objects from the database.
@@ -24,4 +27,20 @@ async def get_all_dreams(
     Returns:
         List[HistoryModel]: List of the dream objects from the database.
     """
-    return await historydao.get_all_dreams(limit=limit, offset=offset, user_id=user_id)
+    return await historydao.get_all_dreams(limit=limit, offset=offset, user_id=user.id)
+
+
+@router.post("/ask-a-dream")
+async def ask_a_dream(
+    dream: str,
+    interpretation: str,
+    user: UserModel = Depends(current_user),
+    historydao: HistoryDAO = Depends(),
+    ) -> None:
+    """Create a dream history.
+
+    Args:
+        new_dream (HistoryCreate): new dream object.
+        historydao (HistoryDAO, optional): DAO for History. Defaults to Depends().
+    """
+    await historydao.create_dream_history(dream=dream, interpretation=interpretation, user_id=user.id)
